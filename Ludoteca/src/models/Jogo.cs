d5947847b1a769162296db5e790f26569cbf6d10
Junior Soares <jjwsoares@outlook.com>
2025-09-12 13:32:24 -0300

    Altera classe Jogo para ser pública e adiciona propriedades, construtor e metodos

--- Ludoteca/src/models/Jogo.cs
+++ Ludoteca/src/models/Jogo.cs
@@ -6,7 +6,34 @@ using System.Threading.Tasks;
 
 namespace Ludoteca.src.models
 {
-    internal class Jogo
+    public class Jogo
     {
+        public Guid Id { get; private set; } = Guid.NewGuid();
+        public string Nome { get; private set; }
+        public string Categoria { get; private set; }
+        public bool EstaEmprestado { get; private set; }
+
+        public Jogo(string nome, string categoria) 
+        { 
+            if (string.IsNullOrWhiteSpace(nome))
+                throw new ArgumentException("O nome do jogo não pode ser vazio ou nulo.", nameof(nome));
+
+            if (string.IsNullOrWhiteSpace(categoria))
+                throw new ArgumentException("A categoria do jogo não pode ser vazio ou nulo.", nameof(categoria));
+
+            Nome = nome;
+            Categoria = categoria;  
+            EstaEmprestado = false;
+        }
+
+        public void Emprestar()
+        {
+            EstaEmprestado = true;
+        }
+
+        public void Devolver()
+        {
+            EstaEmprestado = false;
+        }
     }
 }
