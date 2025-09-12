0e8d5bbfbae0e6a19459198950899f6bf539b622
Junior Soares <jjwsoares@outlook.com>
2025-09-12 13:38:18 -0300

    Alterar classe Membro para pública e adiciona propriedades e o construtor

--- Ludoteca/src/models/Membro.cs
+++ Ludoteca/src/models/Membro.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace Ludoteca.src.models
 {
-    internal class Membro
+    public class Membro
     {
+        public Guid Id { get; private set; } = Guid.NewGuid();
+        public string Nome { get; private set; }
+        public string Matricula { get; private set; }
+        public Membro(string nome, string matricula)
+        {
+            if (string.IsNullOrWhiteSpace(nome))
+                throw new ArgumentException("O nome do membro não pode ser vazio ou nulo.", nameof(nome));
+
+            if (string.IsNullOrWhiteSpace(matricula))
+                throw new ArgumentException("A matricula do membro não pode ser vazio ou nulo.", nameof(matricula));
+
+            Nome = nome;
+            Matricula = matricula;
+        }
     }
 }
