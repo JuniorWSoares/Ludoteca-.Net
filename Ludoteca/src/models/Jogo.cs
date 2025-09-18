2e64a66b816b843b7e659f2f5bfaf95a6a025280
Junior Soares <jjwsoares@outlook.com>
2025-09-18 13:45:14 -0300

    Adiciona suporte à serialização JSON na classe Jogo
    Implementa a diretiva `using System.Text.Json.Serialization;` para permitir a utilização de atributos de serialização JSON. Um novo construtor foi adicionado, marcado com `[JsonConstructor]`, que permite a desserialização de objetos JSON. O construtor original foi mantido para validações.

--- Ludoteca/src/models/Jogo.cs
+++ Ludoteca/src/models/Jogo.cs
@@ -2,18 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Ludoteca.src.models
 {
     public class Jogo
     {
-        public Guid Id { get; private set; }
-        public string Nome { get; private set; }
-        public string Categoria { get; private set; }
-        public bool EstaEmprestado { get; private set; }
+        public Guid Id { get; private set; } //[AV1-2] encapsulamento
+        public string Nome { get; private set; } //[AV1-2] encapsulamento
+        public string Categoria { get; private set; } //[AV1-2] encapsulamento
+        public bool EstaEmprestado { get; private set; } //[AV1-2] encapsulamento
 
-        public Jogo(string nome, string categoria) 
+        // Construtor utilizado pelo System.Text.Json durante a desserialização
+        [JsonConstructor]
+        public Jogo(Guid id, string nome, string categoria, bool estaEmprestado) 
+        { 
+            Id = id;
+            Nome = nome;
+            Categoria = categoria;  
+            EstaEmprestado = estaEmprestado;
+        }
+        public Jogo(string nome, string categoria) //[AV1-2] Construtor com validações
         { 
             if (string.IsNullOrWhiteSpace(nome))
                 throw new ArgumentException("O nome do jogo não pode ser vazio ou nulo.", nameof(nome));
